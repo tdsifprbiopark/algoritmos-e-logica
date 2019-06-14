a8328007fc9716b42a3ec6d6f7da69361357b619
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-14 10:19:41 -0300

    Create exercicio10.pas

---
+++ exercicios/pascal/estruturas-de-repeticao/exercicio10.pas
@@ -0,0 +1,7 @@
+{
+Escreva um algoritmo que leia 10 números e informe:
+A) Quantidade de números digitados maiores ou igual a 0.
+B) Quantidade de números digitados menores que 0.
+C) Soma de todos os números digitados maiores que zero.
+D) Soma dos números digitados menores que zero.
+}
