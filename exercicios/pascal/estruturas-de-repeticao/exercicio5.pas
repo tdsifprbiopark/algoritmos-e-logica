4711f12d468d686df4e9fe888f7ea5e233a4a889
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-14 10:17:34 -0300

    Create exercicio5.pas

---
+++ exercicios/pascal/estruturas-de-repeticao/exercicio5.pas
@@ -0,0 +1,8 @@
+{
+Exercício 5 que na verdade é o 4.
+
+Faça um algoritmo (Para ... até...repetir ... ) que leia 10 valores inteiros e:
+• Encontre e mostre o maior valor
+• Encontre e mostre o menor valor
+• Calcule e mostre a média dos números lidos
+}
