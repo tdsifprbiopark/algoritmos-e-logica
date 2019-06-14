92ef06fb59c2d2196eb4bbdf94d46bd002c3e531
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-14 10:15:41 -0300

    Create exercicio3.pas

---
+++ exercicios/pascal/estruturas-de-repeticao/exercicio3.pas
@@ -0,0 +1,7 @@
+{
+Faça um algoritmo (com a estrutura REPETIR...ATE) que leia 20 valores inteiros e:
+– Encontre e mostre o maior valor;
+– Encontre e mostre o menor valor;
+– Calcule e mostre a média dos números lidos;
+}
+
