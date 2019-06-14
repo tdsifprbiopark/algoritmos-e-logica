e6366b57052d9a3008d36f1067acb41e590f0a83
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-14 10:18:52 -0300

    Create exercicio8.pas

---
+++ exercicios/pascal/estruturas-de-repeticao/exercicio8.pas
@@ -0,0 +1,5 @@
+{
+Escreva um algoritmo que leia vários números e informe quantos números
+entre 100 e 200 foram digitados. Quando o valor 0 (zero) for lido, o algoritmo
+deverá cessar sua execução.
+}
