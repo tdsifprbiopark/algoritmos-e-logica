0ec633877244474e657d7d3fd42e603d50e70afa
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-18 16:41:56 -0300

    Update exercicio06.pas

--- exercicios/pascal/estruturas-de-repeticao/exercicio06.pas
+++ exercicios/pascal/estruturas-de-repeticao/exercicio06.pas
@@ -1,3 +1,18 @@
 {
 Faça um algoritmo que exiba na tela os números ímpares entre 100 e 300.
 }
+
+program Imparehocao;
+
+uses crt;
+
+var 
+nImpar : Integer;
+
+begin
+    Writeln ('Digite qualquer tecla para saber os numeros impares entre 100 e 300');
+    Readkey;
+    For nImpar := 100 to 300 do
+      If nImpar mod 2 = 1 then
+      Writeln (nImpar);
+end.
