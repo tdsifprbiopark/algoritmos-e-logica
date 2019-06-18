df9371009345bf12e68f03f4d2ea7013fb2e4573
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-18 17:06:22 -0300

    Update exercicio07.pas

--- exercicios/pascal/estruturas-de-repeticao/exercicio07.pas
+++ exercicios/pascal/estruturas-de-repeticao/exercicio07.pas
@@ -1,3 +1,18 @@
 {
 Faça um algoritmo que liste todos os múltiplos de 5 no intervalo de 1 a 200.
 }
+
+program Cincao;
+
+uses crt;
+
+var 
+nMultiplo : Integer;
+
+begin
+    Writeln ('Digite qualquer tecla para saber os numeros multiplos de 5 no intervalo 1-200');
+    Readkey;
+    For nMultiplo := 1 to 200 do
+      If nMultiplo mod 5 = 0 then
+      Writeln (nMultiplo);
+end.
