88401da716829741d8bfedac0798a4eb62eb3b14
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:24:16 -0300

    Create exercicio10.pas

---
+++ exercicios/pascal/vetores/exercicio10.pas
@@ -0,0 +1,25 @@
+{
+Jogo da Velha – Crie um programa que permita a duas pessoas jogar o jogo da
+velha. Para isso, utilize uma matriz (ou seja, um vetor bidimensional) com três
+linhas e três colunas, como no tabuleiro do jogo da velha. Cada elemento da
+matriz deveria ser inicializado com um asterisco (“*”) e o programa deveria
+executar um laço para implementar o seguinte comportamento:
+a) O tabuleiro deve ser mostrado na tela (ou seja, a matriz deve ser
+impressa);
+b) O primeiro jogador deve selecionar uma posição no tabuleiro para
+marcar sua posição com um “X”. Para isso, o programa deve perguntar a
+esse jogador qual é a linha e a coluna da posição em que ele pretende
+jogar;
+c) Da mesma forma que o primeiro jogador, o programa deve perguntar ao
+segundo jogador qual é a linha e a coluna da posição em que ele pretende
+marcar com um “O”;
+d) O programa deve verificar se algum dos jogadores ganhou ou se houve
+um empate e, caso uma dessas situações tenha ocorrido, o programa
+deveria informar isso e encerrar. Naturalmente, em caso de uma vitória,
+o programa informa quem ganhou;
+e) O primeiro jogador ganha a partida se conseguir marcar três “X” numa
+única linha ou coluna, ou então na diagonal do tabuleiro. O segundo
+jogador ganha se fizer o mesmo com três “O”. Um empate ocorre quando
+todas as posições do tabuleiro estão preenchidas sem que ninguém
+tenha vencido.
+}
