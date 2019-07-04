a42ff9eb66fffa0018001cfa19743e298ce7e595
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:11:51 -0300

    Create exercicio08.pas

---
+++ exercicios/pascal/vetores/exercicio08.pas
@@ -0,0 +1,15 @@
+{
+Folha de pagamento – Imagine que você esteja desenvolvendo um programa
+para processar a folha de pagamento de uma empresa que possui 10
+empregados. Para isso, você deve utilizar vetores paralelos com 10 posições para
+armazenar as seguintes informações:
+Vetor 1 - Código do Empregado: códigos numéricos para identificar cada
+um dos empregados. Esses códigos são informados abaixo:
+56588 45201 78951 87775 84512
+13028 75804 92057 69403 29625
+Vetor 2 - Horas Trabalhadas: o número de horas trabalhadas por cada
+um dos 10 empregados num determinado mês;
+Vetor 3 - Valor da Hora Trabalhada: o valor da hora trabalhada de cada
+empregado;
+Vetor 4 - Valor dos Salários: o salário bruto de cada empregado.
+}
