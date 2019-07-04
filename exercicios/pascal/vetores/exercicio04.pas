d8aadc29100524f4e2127a8305ca2d2aa0ff9fd2
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:09:39 -0300

    Create exercicio04.pas

---
+++ exercicios/pascal/vetores/exercicio04.pas
@@ -0,0 +1,10 @@
+{
+Estatística descritiva – Crie um algoritmo que leia continuamente vários
+números informados pelo usuário e os armazene num vetor, até que o usuário
+digite um valor de sentinela (como -999) ou tenha informado 20 números (o que
+acontecer antes). Depois disso, exiba as seguintes estatísticas:
+a) O menor número do vetor;
+b) O maior número;
+c) A quantidade total de números que o usuário informou;
+d) A média dos números fornecidos.
+}
