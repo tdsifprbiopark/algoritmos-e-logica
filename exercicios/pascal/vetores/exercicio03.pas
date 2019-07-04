21baae9cbba7041d73149ff1bc70e12ae32fa441
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-03 22:10:38 -0300

    Create exercicio03.pas

---
+++ exercicios/pascal/vetores/exercicio03.pas
@@ -0,0 +1,10 @@
+{
+Estatísticas pluviométricas – o índice pluviométrico é uma medida em
+milímetros que resulta da quantidade total da precipitação de água num
+determinado local. Sabendo disso, escreva um algoritmo que permita ao usuário
+informar o índice pluviométrico de cada mês do ano. Após ler esses dados, o
+algoritmo deve exibir qual foi a precipitação total no ano (ou seja, a soma do
+índice pluviométrico de cada mês), a média de precipitação e quais foram os
+meses com os valores mais alto e mais baixo de precipitação (ou seja, quais
+foram os meses em que choveu mais e menos no ano).
+}
