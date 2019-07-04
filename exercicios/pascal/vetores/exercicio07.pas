a92d872647dc722812f05f989f859cd9ac36bd57
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:11:10 -0300

    Create exercicio07.pas

---
+++ exercicios/pascal/vetores/exercicio07.pas
@@ -0,0 +1,9 @@
+{
+Lista telefônica – Utilizando dois vetores paralelos, crie um algoritmo que
+armazene o nome de 7 pessoas num deles e os números de telefone dessas
+pessoas no outro (essas informações devem ser fornecidas pelo usuário). Ao
+final, o algoritmo deve perguntar ao usuário informe o nome de uma pessoa e
+exibir o número dela. Caso esse nome não exista, deve-se exibir uma mensagem
+de erro. Faça um laço para que o usuário possa indicar se deseja continuar
+pesquisando por mais números de telefone ou se deseja encerrar o algoritmo.
+}
