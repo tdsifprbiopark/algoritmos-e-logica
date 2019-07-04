4596223cfac784df3b87a3161a72d845ab0ada14
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:13:03 -0300

    Create exercicio09.pas

---
+++ exercicios/pascal/vetores/exercicio09.pas
@@ -0,0 +1,21 @@
+{
+Exame de Habilitação para CNH – Para tirar a Carteira Nacional de Habilitação
+(CNH), é necessário realizar um exame teórico, onde o candidato responde a 20
+questões objetivas de uma prova, cada uma delas possuindo 4 alternativas (“A”,
+“B”, “C”, “D” e “E”) como resposta. Imagine que você esteja escrevendo um
+programa para automatizar essa prova e, para isso, precisa pedir que o candidato
+informe qual é a resposta que ele deu para cada uma das questões. As respostas
+corretas são dadas abaixo:
+
+01.B 06.A 11.B 16.C
+02.D 07.B 12.C 17.C
+03.A 08.A 13.D 18.B
+04.A 09.C 14.A 19.D
+05.C 10.D 15.D 20.A
+
+O seu programa deve armazenar as respostas corretas num vetor e as respostas
+do candidato em outro vetor. Quando o usuário acabar a prova, o programa deve
+corrigi-la, dizendo se o usuário foi aprovado ou não (é necessário acertar no
+mínimo 15 questões para ser aprovado) e exibir uma listagem das questões que
+foram respondidas incorretamente.
+}
