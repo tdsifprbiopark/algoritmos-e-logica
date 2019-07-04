1ac9f6ef936299b164fc9c50ce222b7465372fd9
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:10:45 -0300

    Create exercicio06.pas

---
+++ exercicios/pascal/vetores/exercicio06.pas
@@ -0,0 +1,19 @@
+{
+Quantidade de dias em cada mês – Construa um algoritmo que mostre o
+número de dias em cada mês, imprimindo na tela um relatório como o mostrado
+abaixo:
+Janeiro tem 31 dias.
+Fevereiro tem 28 dias.
+Março tem 31 dias.
+Abril tem 30 dias.
+Maio tem 31 dias.
+Junho tem 30 dias.
+Julho tem 31 dias.
+Agosto tem 31 dias.
+Setembro tem 30 dias.
+Outubro tem 31 dias.
+Novembro tem 30 dias.
+Dezembro tem 31 dias.
+Para resolver esse problema, utilize dois vetores paralelos, um para o nome dos
+meses e outro para a quantidade de dias.
+}
