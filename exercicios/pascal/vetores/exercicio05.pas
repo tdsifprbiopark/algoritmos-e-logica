3e64cf5644f87c85d384f0482210e5422f259388
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-04 11:10:21 -0300

    Create exercicio05.pas

---
+++ exercicios/pascal/vetores/exercicio05.pas
@@ -0,0 +1,10 @@
+{
+Validação de número de conta - Crie um algoritmo que peça ao usuário para
+fornecer um número de conta e determine se esse número é válido. Para ser
+válido, esse número deve ser igual a um dos listados abaixo:
+5658845 4520125 7895122 8777541 8451277 1302850
+8080152 4562555 5552012 5050552 7825877 1250255
+1005231 6545231 3852085 7576651 7881200 4581002
+Se o número fornecido pelo usuário estiver nessa lista, o algoritmo deverá
+informar que o número é válido; caso contrário, deverá informar que é inválido.
+}
