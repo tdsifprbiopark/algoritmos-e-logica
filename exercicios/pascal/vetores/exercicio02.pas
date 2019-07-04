8a253931024c9cc3439bacbf56b654f0ddb612f6
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-03 22:08:55 -0300

    Create exercicio02.pas

---
+++ exercicios/pascal/vetores/exercicio02.pas
@@ -0,0 +1,14 @@
+{
+Gerador de Números de Loteria – Crie um algoritmo que gere o número de um
+bilhete de loteria de acordo com as seguintes instruções:
+a) O número deverá ter 7 dígitos;
+b) Cada dígito deve ser armazenado numa posição de um vetor criado por
+você (ou seja, esse vetor terá que ter 7 posições);
+c) Para gerar o número, você deve preencher cada posição do vetor com
+um número aleatório entre 0 e 9;
+d) Ao final, exiba o número gerado.
+Dica: para gerar um dígito aleatório, utilize a função random(9). Para que essa
+função gere números verdadeiramente aleatórios, utilize a procedure
+randomize logo no início do programa (antes do laço que você utilizará para
+preencher o vetor com o número gerado).
+}
