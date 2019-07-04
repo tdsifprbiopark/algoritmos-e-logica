493a40e8d09745b160ec777757f5584940e6878a
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-07-03 22:07:35 -0300

    Create exercicio01.pas

---
+++ exercicios/pascal/vetores/exercicio01.pas
@@ -0,0 +1,9 @@
+{
+Total de vendas de uma loja – Escreva um algoritmo que pergunte ao usuário
+qual foi o total vendido (em reais) a cada dia da semana (identificado por um
+número de 1 a 7), armazenando os dados num vetor. Ao final, utilize esse vetor
+para determinar o total vendido na semana e escreva esse valor na tela.
+Feito isso, modifique o algoritmo para que, ao perguntar o total vendido em cada
+dia da semana, ele informe de qual dia se trata, por extenso (“segunda feira”,
+“terça feira”, “quarta feira”, etc).
+}
