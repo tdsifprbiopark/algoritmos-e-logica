c4fc2516c5aeaddc785de4461595f6b946098b75
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-08-08 21:43:06 -0300

    Create desafio1.pas

---
+++ exercicios/pascal/estruturas-de-repeticao/08_08_2019/desafio1.pas
@@ -0,0 +1,16 @@
+{
+1. Utilizando apenas laços de repetição e estruturas de decisão faça:
+(A) Crie um algoritmo para realizar a  criptografia de uma senha, leve em consideração as seguintes informações:
+  - A senha só poderá ter 20 caracteres;
+  - A senha precisa ter mais de 9 caracteres;
+  - Possuir ao menos uma letra maiúscula;
+  - Possuir ao menos uma letra minúscula;
+  - Possuir um número ímpar;
+  - Possuir um número par;
+  - Possuir um carácter especial;
+  - O algoritmo só deverá ser encerrado após a senha ser criptografada e exibida.
+(B) Utilizando a senha criptografada na "A" faça o procedimento de dscriptografia da senha.
+
+enviar para thiago.cosseau@edu.unipar.br
+
+}
